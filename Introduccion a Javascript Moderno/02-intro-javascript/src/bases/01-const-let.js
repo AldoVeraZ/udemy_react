9dfa9c04fa6f151c2f658a1fc7f58409b42dd50e
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2024-10-18 15:33:01 -0300

    const and let class 01

---
+++ Introduccion a Javascript Moderno/02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,23 @@
+console.log("Hola Mundo!!!");
+
+// Variables y Constantes
+
+const nombre = "Aldo";
+const apellido = "Vera";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+// var No se debe usar...
+if (true) {
+  const nombre = "Krusty";
+
+  console.log(nombre);
+  /*   let valorDado = 6; */
+
+  /* console.log(valorDado); */
+}
+
+console.log(valorDado);
