cb2dc9defa0f927a402331fd276ee68e189de349
Aldo Esteban Vera Zúñiga <37416336+AldoVeraZ@users.noreply.github.com>
2024-12-19 22:43:14 -0300

    Delete Gif Expert App/04-gif-expert-app directory

--- Gif Expert App/04-gif-expert-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { GifExpertApp } from "./GifExpertApp";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <GifExpertApp />
-  </StrictMode>
-);
