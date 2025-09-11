8e653d833d4b6babc8261ab1296bdf4fbd19104d
Aldo Esteban Vera Zúñiga <aestebanveraz@gmail.com>
2025-09-11 19:09:12 -0300

    uploading the first funtional components of superheroes app

---
+++ Single Page Aplication SPA/05-heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,14 @@
+import { CustomJumbotron } from "@/components/Custom/CustomJumbotron";
+
+export const SearchPage = () => {
+  return (
+    <>
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descubre, explora y administra héroes y villanos"
+      />
+    </>
+  );
+};
+
+export default SearchPage;
